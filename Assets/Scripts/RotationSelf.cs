e7f7c369fe18a6e901ff2c43591a9118cd6e22a2
nothing <514526588@qq.com>
2020-08-14 16:23:54 +0800

    bug fixed

---
+++ Assets/Scripts/RotationSelf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationSelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * 90 * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Debug.Log("GetMouseButtonDown");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("点击了3D角色");
+    }
+}
